76b51aa78230bc2a9e44529143db54ad4ef0c327
João Pedro Lourenço dos Santos <joao.santos1776@etec.sp.gov.br>
2024-06-11 23:02:28 -0300

    inicio da tela de estoque

--- src/components/Estoque.js
+++ src/components/Estoque.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "./Estoque.css";
 
 const Estoque = () => {
   return (
     <div className="container estoque">
       <div className="title">
         <h1>Estoque</h1>
+      </div>
+
+      <div className="estoqueCadastroItem">
+        <form>
+          <input type="text" placeholder="Nome do item" />
+          <input type="text" placeholder="Qtd em estoque" />
+          <input type="submit" value="Confirmar" />
+        </form>
       </div>
     </div>
   );
